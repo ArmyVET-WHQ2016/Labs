e8a18f9e29f553f12cca013df3efb4251ec6a003
Wendy Hamiltonquinn <wendyhamiltonquinn@outlook.com>
2022-05-12 19:26:36 -0400

    updates to VS Code

---
+++ app.js
@@ -0,0 +1,11 @@
+let loginName = prompt("What's your loginName, love?");
+             
+        document.write(loginName);
+
+    loginName = prompt("What is your current loginName");
+
+        document.write(loginName);
+
+    loginName = prompt("What is your loginName again");
+
+        document.write(loginName);
