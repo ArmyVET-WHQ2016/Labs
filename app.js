f982b346dea38f146b17d11ee90a9f50b20b31ea
Wendy Hamiltonquinn <wendyhamiltonquinn@outlook.com>
2022-05-17 18:10:35 -0400

    add functions

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ function writeName(){
     let vacationName = prompt("What's your favorite vacation spot?");
     document.write(vacationName);
 }
+function guessMyAge(){
+    let userAge = prompt("What is my age?"); 
+        if (userAge !== "49"){
+            alert("wrong");
+            guessMyAge();
+        } else if (userAge==="49") {
+            alert("right");
+        } 
+    
+}
