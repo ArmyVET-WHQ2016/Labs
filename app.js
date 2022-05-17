56763a11d764dac10dc3c03e6cffc261dccb0197
Wendy Hamiltonquinn <wendyhamiltonquinn@outlook.com>
2022-05-17 16:14:05 -0400

    created function

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-let loginName = prompt("What's your loginName, love?");
-document.write(loginName);
-let Hawaii = prompt("Have you ever been to Hawaii?");
-alert("The most beautiful place to visit")  
-
-
+function writeName(){
+    let vacationName = prompt("What's your favorite vacation spot?");
+    document.write(vacationName);
+}
